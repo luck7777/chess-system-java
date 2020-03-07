55bbb8c6fb64d974b01b742f8ce5652a0e1cb58c
Lucas Araujo <ljca84@gmail.com>
2020-03-07 13:24:22 -0300

    commit teste pelo eclipse no e-git

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-
+     // teste
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
