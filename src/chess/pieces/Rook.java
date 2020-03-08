6305227d70f4883dc18300b52712ca703eab24a6
Lucas Araujo <ljca84@gmail.com>
2020-03-08 09:12:48 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -55,6 +55,9 @@ public class Rook extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() + 1);
 		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
 
 		return mat;
 	}
