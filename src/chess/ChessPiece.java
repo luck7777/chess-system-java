0119b8fee2b723ae817d78f242524aab9ed52abc
Lucas Araujo <ljca84@gmail.com>
2020-03-16 07:03:37 -0300

    Bishop

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,30 +5,36 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
-	private Integer moveCount;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
+
 	public Color getColor() {
 		return color;
 	}
+	
 	public int getMoveCount() {
 		return moveCount;
 	}
+	
 	public void increaseMoveCount() {
 		moveCount++;
 	}
+
 	public void decreaseMoveCount() {
 		moveCount--;
 	}
-	public ChessPosition getChessPosition () {
+
+	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
-	protected boolean isThereOpponentPiece (Position position) {
+	
+	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
